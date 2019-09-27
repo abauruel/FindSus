01ca7be485dba2e3c7b425a9f22e17a363f20015
abauruel@gmail.com <Bauruel3>
2019-09-27 12:05:38 -0300

    ajuste para mac

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
-import Screen from './pages/Screen';
+//import Screen from './pages/Screen';
 // import Login from './pages/Login';
 import Home from './pages/Home';
 
