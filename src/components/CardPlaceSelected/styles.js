c6eb776bbdafa0cd443dbe403f564c5f4cc426e5
abauruel <abauruel@gmail.com>
2020-02-07 20:50:57 +0000

    added style

--- src/components/CardPlaceSelected/styles.js
+++ src/components/CardPlaceSelected/styles.js
@@ -1,3 +1,13 @@
 import styled from 'styled-components/native';
+import LinearGradient from 'react-native-linear-gradient';
 
-export const Container = styled.View``;
+export const LinearColor = styled(LinearGradient).attrs({
+  colors: ['#FFF', 'rgba(255,255,255,0.0)'],
+  locations: [0.75, 1],
+
+  start: {x: 1, y: 0},
+  end: {x: 1, y: 1},
+})`
+  border-radius: 8px;
+  margin-top: 0px;
+`;
