44159c6987fd0bc43a675541157957b5b4fa6c5a
abauruel <abauruel@gmail.com>
2020-02-08 01:03:38 +0000

    New  style create

--- src/components/CardPlaceSelected/index.js
+++ src/components/CardPlaceSelected/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
-import {View, Text, Linking, TouchableOpacity} from 'react-native';
+import {View, Text, Linking} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome5';
-import {LinearColor} from './styles';
+import {
+  LinearColor,
+  Card,
+  CardContent,
+  Name,
+  ContainerButton,
+  ContainerContent,
+  ContainerDistance,
+  TitleDistance,
+  ButtonRedirect,
+  TextButtonredirect,
+} from './styles';
 
 export default function CardPlaceSelected({
   estabelecimentos,
@@ -11,13 +22,11 @@ export default function CardPlaceSelected({
 }) {
   return (
     <LinearColor>
-      <View style={{flexDirection: 'row', paddingLeft: 20, paddingTop: 10}}>
+      <Card>
         <Icon name="hospital-alt" size={30} color="#000" style={{margin: 15}} />
-        <View style={{marginRight: 30}}>
-          <Text style={{fontSize: 18, fontWeight: 'bold', maxWidth: 290}}>
-            {estabelecimentos[calloutSelect].nome}
-          </Text>
-          <View style={{flexDirection: 'row'}}>
+        <CardContent>
+          <Name>{estabelecimentos[calloutSelect].nome}</Name>
+          <ContainerContent>
             <View>
               <Text>{estabelecimentos[calloutSelect].logradouro}</Text>
               <Text>{estabelecimentos[calloutSelect].bairro}</Text>
@@ -27,40 +36,28 @@ export default function CardPlaceSelected({
                 {estabelecimentos[calloutSelect].uf}
               </Text>
               <Text>{estabelecimentos[calloutSelect].cep}</Text>
-              <View
-                style={{
-                  flexDirection: 'row',
-                  justifyContent: 'flex-start',
-                  alignItems: 'center',
-                  paddingTop: 5,
-                }}>
+              <ContainerDistance>
                 {distance && (
                   <Icon name="car" size={12} style={{marginRight: 10}} />
                 )}
-                <Text style={{marginRight: 20}}>
+                <TitleDistance>
                   {distance ? `${distance} km` : '-'}
-                </Text>
-              </View>
+                </TitleDistance>
+              </ContainerDistance>
             </View>
-            <View style={{justifyContent: 'center'}}>
-              <TouchableOpacity
-                style={{
-                  backgroundColor: '#1BABE3',
-                  elevation: 3,
-                  borderRadius: 100,
-                  padding: 20,
-                }}
+            <ContainerButton>
+              <ButtonRedirect
                 onPress={() =>
                   Linking.openURL(
                     `geo:${region}?q=${estabelecimentos[calloutSelect].latitude},${estabelecimentos[calloutSelect].longitude}`,
                   )
                 }>
-                <Text style={{color: '#FFF'}}> IR </Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-        </View>
-      </View>
+                <TextButtonredirect> IR </TextButtonredirect>
+              </ButtonRedirect>
+            </ContainerButton>
+          </ContainerContent>
+        </CardContent>
+      </Card>
     </LinearColor>
   );
 }
--- src/components/CardPlaceSelected/styles.js
+++ src/components/CardPlaceSelected/styles.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components/native';
 import LinearGradient from 'react-native-linear-gradient';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 export const LinearColor = styled(LinearGradient).attrs({
   colors: ['#FFF', 'rgba(255,255,255,0.0)'],
@@ -10,4 +11,46 @@ export const LinearColor = styled(LinearGradient).attrs({
 })`
   border-radius: 8px;
   margin-top: 0px;
+`;
+
+export const Card = styled.View`
+  flex-direction: 'row';
+  padding-left: 20;
+  padding-top: 10;
+`;
+export const Cardicon = styled(Icon)``;
+export const CardContent = styled.View`
+  margin-right: 30;
+`;
+export const Name = styled.Text`
+  font-size: 18;
+  font-weight: 'bold';
+  max-width: 290;
+`;
+
+export const ContainerContent = styled.View`
+  flex-direction: 'row';
+`;
+export const ContainerDistance = styled.View`
+  flex-direction: 'row';
+  justify-content: 'flex-start';
+  align-items: 'center';
+  padding-top: 5;
+`;
+
+export const TitleDistance = styled.Text`
+  margin-right: 20;
+`;
+
+export const ContainerButton = styled.View`
+  justify-content: 'center';
+`;
+export const ButtonRedirect = styled.TouchableOpacity`
+  background: '#1BABE3';
+  elevation: 3;
+  border-radius: 100;
+  padding: 20;
+`;
+export const TextButtonredirect = styled.Text`
+  color: #fff;
 `;
